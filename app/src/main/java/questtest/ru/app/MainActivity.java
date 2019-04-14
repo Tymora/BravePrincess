485e0a5dcbb8a06582bbec827a5e1537b2351512
Tymora <dachabel@mail.ru>
2019-04-14 14:45:51 +0300

    One_level

--- app/src/main/java/questtest/ru/app/MainActivity.java
+++ app/src/main/java/questtest/ru/app/MainActivity.java
@@ -1,7 +1,10 @@
 package questtest.ru.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button buttonmeny = (Button) findViewById(R.id.menuButton);
+        buttonmeny.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonmeny.setBackgroundResource(R.drawable.buttonmeny);
+                try {
+                    Intent intent = new Intent(MainActivity.this, One_level.class);startActivity(intent);finish();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/questtest/ru/app/One_level.java
@@ -0,0 +1,18 @@
+package questtest.ru.app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class One_level extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.one_level);
+
+
+
+            }
+        }
