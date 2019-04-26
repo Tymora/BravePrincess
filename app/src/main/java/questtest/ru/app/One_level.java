38fb6fc866fa0147db6f61c8eb48f5bd7fc34078
Tymora <dachabel@mail.ru>
2019-04-26 17:21:29 +0300

    Доделала первый уровень ( еще нужно пофиксить кнопку)

--- app/src/main/java/questtest/ru/app/One_level.java
+++ app/src/main/java/questtest/ru/app/One_level.java
@@ -4,15 +4,35 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+
+        i
 public class One_level extends AppCompatActivity {
+
+
+
+    OneTable oneTable = new OneTable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.one_level);
+        final TextView textView1 = (TextView) findViewById(R.id.textView1);
+        final TextView textView2 = (TextView) findViewById(R.id.textView2);
+        final TextView textView3 = (TextView) findViewById(R.id.textView3);
+        final TextView textView4 = (TextView) findViewById(R.id.textView4);
+        final TextView textView5 = (TextView) findViewById(R.id.textView5);
+        final TextView textView6 = (TextView) findViewById(R.id.textView6);
+        final TextView textView7 = (TextView) findViewById(R.id.textView7);
+        final TextView textView8 = (TextView) findViewById(R.id.textView8);
+        final TextView textView9 = (TextView) findViewById(R.id.textView9);
+        final TextView textView10 = (TextView) findViewById(R.id.textView10);
+        final TextView textView11 = (TextView) findViewById(R.id.textView11);
+        final Button one_button_next_level = (Button)findViewById(R.id.one_button_next_level);
 
 
+        textView1.setText(oneTable.onescenario[0]);
 
-            }
+    }
         }
