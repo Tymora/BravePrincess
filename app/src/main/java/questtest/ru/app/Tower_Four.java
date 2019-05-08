4b677d6f727a3002273600a4b91128caeb232b69
Tymora <dachabel@mail.ru>
2019-05-08 18:51:11 +0300

    Добавила новые уровни ( их надо заполнить)

---
+++ app/src/main/java/questtest/ru/app/Tower_Four.java
@@ -0,0 +1,14 @@
+package questtest.ru.app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Tower_Four extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tower_four);
+
+    }
+}
---
+++ app/src/main/java/questtest/ru/app/Tower_Six.java
@@ -0,0 +1,14 @@
+package questtest.ru.app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Tower_Six extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tower_six);
+
+    }
+}
