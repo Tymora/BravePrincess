c74ac4c3a8f9781f0b87f42b27dcb758c624e5bb
Tymora <dachabel@mail.ru>
2019-05-13 18:16:09 +0300

    Попытка исправить кнопку

--- app/src/main/java/questtest/ru/app/Tower_Two.java
+++ app/src/main/java/questtest/ru/app/Tower_Two.java
@@ -1,7 +1,10 @@
 package questtest.ru.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Tower_Two extends AppCompatActivity {
 
@@ -9,6 +12,15 @@ public class Tower_Two extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tower_two);
+        final Button buttonmeny = (Button) findViewById(R.id.menuButton);
+        buttonmeny.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button_one.setBackgroundResource(R.drawable.button_one);
+                try {
+                    Intent intent = new Intent(Tower_Two.this, Tower_One.class);startActivity(intent);finish();
+                } catch (Exception e) {
+                    e.printStackTrace();
 
     }
 }
